01348fe04c3fa24b56f8554465e8291677855b23
perkal512 <msegal957@gmail.com>
2025-12-09 22:25:13 +0200

    Add Models and Data folders with Garage.cs and AppDbContext.cs

--- GaragesApi/Data/AppDbContext.cs
+++ GaragesApi/Data/AppDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace GaragesApi.Data
+﻿using GaragesApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace GaragesApi.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Garage> Garages { get; set; }
     }
 }
--- GaragesApi/Models/Garage.cs
+++ GaragesApi/Models/Garage.cs
@@ -1,6 +1,29 @@
-﻿namespace GaragesApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GaragesApi.Models
 {
     public class Garage
     {
+        [Key]
+        public int GarageId { get; set; }
+
+        [Required]
+        public int ExternalId { get; set; }
+
+        public int MisparMosah { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int CodSugMosah { get; set; }
+        public string SugMosah { get; set; } = string.Empty;
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Phone { get; set; }
+        public int Mikud { get; set; }
+        public int CodMiktzoa { get; set; }
+        public string? Profession { get; set; }
+        public string? Manager { get; set; }
+        public long RashamHavarot { get; set; }
+        public string? TestTime { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
