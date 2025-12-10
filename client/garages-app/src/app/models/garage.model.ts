64a6c8036fbbe776ce6e8e060a263d95b9dcf00e
perkal512 <msegal957@gmail.com>
2025-12-10 13:49:14 +0200

    Add Garage model and GarageService for API communication

--- client/garages-app/src/app/models/garage.model.ts
+++ client/garages-app/src/app/models/garage.model.ts
@@ -1,2 +1,18 @@
-export class Garage {
+export interface Garage {
+  garageId?: number;
+  externalId: number;
+  misparMosah: number;
+  name: string;
+  codSugMosah: number;
+  sugMosah: string;
+  address: string;
+  city: string;
+  phone: string;
+  mikud: number;
+  codMiktzoa: number;
+  profession: string;
+  manager: string;
+  rashamHavarot: number;
+  testTime?: string;
+  createdAt?: string;
 }
--- client/garages-app/src/app/services/garages.service.ts
+++ client/garages-app/src/app/services/garages.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Garage } from '../models/garage.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GaragesService {
+export class GarageService {
 
-  constructor() { }
+  private apiUrl = 'https://localhost:5134/api/garages'; 
+
+  constructor(private http: HttpClient) { }
+
+  getAllGarages(): Observable<Garage[]> {
+    return this.http.get<Garage[]>(`${this.apiUrl}/get-all`);
+  }
+
+  getExternalGarages(): Observable<Garage[]> {
+    return this.http.get<Garage[]>(`${this.apiUrl}/garages-from-gov`);
+  }
+
+  addGarage(garage: Garage): Observable<Garage> {
+    return this.http.post<Garage>(`${this.apiUrl}/add`, garage);
+  }
+
+  addMultipleGarages(garages: Garage[]): Observable<Garage[]> {
+    return this.http.post<Garage[]>(`${this.apiUrl}/add-multiple`, garages);
+  }
 }
