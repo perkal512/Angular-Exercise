b5145771fb72c85cfdfd31c28a4589ad5da0346a
perkal512 <msegal957@gmail.com>
2025-12-10 16:56:27 +0200

    Adding CORS permissions

--- GaragesApi/Program.cs
+++ GaragesApi/Program.cs
@@ -13,10 +13,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
